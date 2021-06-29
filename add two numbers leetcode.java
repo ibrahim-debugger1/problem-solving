4e4dc4dafd59a0daef9a3f64135fa7aa97c4355c
ibrahim-debugger1 <86594870+ibrahim-debugger1@users.noreply.github.com>
2021-06-29 18:51:01 +0300

    Update add two numbers leetcode.java

--- add two numbers leetcode.java
+++ add two numbers leetcode.java
@@ -8,6 +8,15 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+*/
+/*
+2. Add Two Numbers
+medium
+*/
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode new_node=new ListNode(0);
