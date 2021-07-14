cb4bd250ecb72c3977faba728301b30776bcc932
ibrahim-debugger1 <86594870+ibrahim-debugger1@users.noreply.github.com>
2021-07-14 17:07:17 +0300

    Update Container With Most Water.cpp

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-    int maxArea(vector<int>& height) {
+//simple question but i can't solve it by my self and i search for an idea to solve it and i do it
+//11. Container With Most Water
+int maxArea(vector<int>& height) {
      int sol=0,l=0,r=height.size()-1;
         while(l<r){
             sol=max(sol,min(height[l],height[r])*(r-l));
