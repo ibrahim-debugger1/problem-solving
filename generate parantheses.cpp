15ef72b0f8178987dcb75ed25b7d4066958e0702
ibrahim-debugger1 <86594870+ibrahim-debugger1@users.noreply.github.com>
2021-07-24 14:10:48 +0300

    Add files via upload

---
+++ generate parantheses.cpp
@@ -0,0 +1,12 @@
+void generateParenthesisByRec(int n,string s,int r,int l){
+        if(r==l && r!=0 && r==n){
+            sol.push_back(s);
+            return ;
+        }
+        if(r<n){
+            generateParenthesisByRec(n,s+"(",r+1,l);
+        }if(l<r){
+            generateParenthesisByRec(n,s+")",r,l+1);
+        }
+        return ;
+    }
