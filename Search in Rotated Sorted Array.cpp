2e6b6a00c7a80909f0f8e8ece283d0bedd468f83
ibrahim-debugger1 <86594870+ibrahim-debugger1@users.noreply.github.com>
2021-08-24 01:11:46 +0300

    Add files via upload

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array/
+//that is a great question i tried my best to solve it but i can't therefor i took the idea from youtube not the code just the idea hehehehehehe
+int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        while(l<r){
+            int mid=(r+l)/2;
+            if(nums[mid]>nums[r])
+                l=mid+1;
+            else
+                r=mid;
+        }
+        int pivot=r;
+        l=0;
+        while(l<=r){
+            int mid=(r+l)/2;
+            if(nums[mid]>target)
+                r=mid-1;
+            else if(nums[mid]<target)
+                l=mid+1;
+            else 
+                return mid;
+        }
+        l=pivot;r=nums.size()-1;
+        while(l<=r){
+            int mid=(r+l)/2;
+            if(nums[mid]>target)
+                r=mid-1;
+            else if(nums[mid]<target)
+                l=mid+1;
+            else 
+                return mid;
+        }
+        return -1;
+    }
