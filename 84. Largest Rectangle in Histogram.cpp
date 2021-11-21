cf87a4a31c27dca5240814e47901737543dd74ef
ibrahim-debugger1 <86594870+ibrahim-debugger1@users.noreply.github.com>
2021-11-21 16:00:13 +0200

    Update 84. Largest Rectangle in Histogram.cpp

--- 84. Largest Rectangle in Histogram.cpp
+++ 84. Largest Rectangle in Histogram.cpp
@@ -1,15 +1,14 @@
-int largestRectangleArea(vector<int>& heights) {
+ int largestRectangleArea(vector<int>& heights) {
         stack<pair<int,int>>st;
-        st.push({0,-1});
         int i=0,sol=0;
         for(;i<heights.size();i++){
-            if(st.size()==1 || heights[i]>=st.top().first){
+            if(st.empty() || heights[i]>=st.top().first){
                 st.push({heights[i],i});
             }else{
-                while(st.size()!=1 && heights[i]<st.top().first){
+                while(!st.empty() && heights[i]<st.top().first){
                     int curr=st.top().first;
                     st.pop();
-                    if(st.size()!=1)
+                    if(!st.empty())
                         sol=max(sol,curr*(i-st.top().second-1));
                     else
                         sol=max(sol,curr*(i));
@@ -17,13 +16,8 @@ int largestRectangleArea(vector<int>& heights) {
                 st.push({heights[i],i});
             }
         }
-        while(st.size()!=1){
+        while(!st.empty()){
             int curr=st.top().first;
-            if(st.size()==2){
-                sol=max(sol,curr*(i));
-                st.pop();
-                continue;
-            }
             st.pop();
             if(!st.empty())
                     sol=max(sol,curr*(i-st.top().second-1));
