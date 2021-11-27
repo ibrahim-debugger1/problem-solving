84b1442b394daaac53f2816598c3570abb3484ad
ibrahim-debugger1 <86594870+ibrahim-debugger1@users.noreply.github.com>
2021-11-27 13:46:18 +0200

    Add files via upload

--- 42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water.cpp
@@ -1,17 +1,16 @@
-int left_max[20005],right_max[20005];
+    //Time complexity= O(n) ,Space complexity=O(1) 
     int trap(vector<int>& height) {
-        int n=height.size();
-        left_max[0]=height[0];
-        for(int i=1;i<n;i++){
-            left_max[i]=max(height[i],left_max[i-1]);
-        }
-        right_max[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            right_max[i]=max(height[i],right_max[i+1]);
-        }
-        int sol=0;
-        for(int i=1;i<n-1;i++){
-            sol+=min(left_max[i],right_max[i])-height[i];
+        int l=0,r=height.size()-1,left_max=height[0],right_max=height[height.size()-1],sol=0;
+        while(l<r){
+            if(right_max>=left_max){
+                sol+=min(left_max,right_max)-height[l];
+                l++;
+                left_max=max(left_max,height[l]);
+            }else{ 
+                sol+=min(left_max,right_max)-height[r];
+                r--;
+                right_max=max(right_max,height[r]);
+            }
         }
         return sol;
     }
