c312f3d8fe4b1245ccbc8f6e1317735a59434395
ibrahim-debugger1 <86594870+ibrahim-debugger1@users.noreply.github.com>
2022-10-03 18:07:46 +0300

    Update Educational Codeforces Round 135.C_problem.cpp

--- Educational Codeforces Round 135.C_problem.cpp
+++ Educational Codeforces Round 135.C_problem.cpp
@@ -10,56 +10,56 @@ int  main() {
 	cin >> t;
 	while (t--) {
 		int n;
-		cin>>n;
-		map<int,int>a,b;
-		for(int i=0;i<n;i++){
+		cin >> n;
+		map<int, int>a, b;
+		for (int i = 0; i < n; i++) {
 			int q;
-			cin>>q;
+			cin >> q;
 			a[q]++;
 		}
-		for(int i=0;i<n;i++){
+		for (int i = 0; i < n; i++) {
 			int q;
-			cin>>q;
+			cin >> q;
 			b[q]++;
 		}
-		for(auto i:a){
-			int num=i.first;
-			int freq1=a[num],freq2=b[num];
-			a[num]-=min(freq2,freq1);
-			b[num]-=min(freq2,freq1);
+		for (auto i : a) {
+			int num = i.first;
+			int freq1 = a[num], freq2 = b[num];
+			a[num] -= min(freq2, freq1);
+			b[num] -= min(freq2, freq1);
 		}
-		int sol=0;
-		for(auto i:a){
-			int num=i.first;
-			if(num>9){
-				int temp=(int)log10(num)+1;
-				a[temp]+=a[num];
-				sol+=a[num];
-				a[num]=0;
+		int sol = 0;
+		for (auto i : a) {
+			int num = i.first;
+			if (num > 9) {
+				int temp = (int)log10(num) + 1;
+				a[temp] += a[num];
+				sol += a[num];
+				a[num] = 0;
 			}
 		}
-		for(auto i:b){
-			int num=i.first;
-			if(num>9){
-				int temp=(int)log10(num)+1;
-				b[temp]+=b[num];
-				sol+=b[num];
-				b[num]=0;
+		for (auto i : b) {
+			int num = i.first;
+			if (num > 9) {
+				int temp = (int)log10(num) + 1;
+				b[temp] += b[num];
+				sol += b[num];
+				b[num] = 0;
 			}
 		}
-		for(auto i:a){
-			int num=i.first;
-			int freq1=a[num],freq2=b[num];
-			a[num]-=min(freq2,freq1);
-			b[num]-=min(freq2,freq1);
+		for (auto i : a) {
+			int num = i.first;
+			int freq1 = a[num], freq2 = b[num];
+			a[num] -= min(freq2, freq1);
+			b[num] -= min(freq2, freq1);
 		}
-		for(auto i:a)
-			if(i.first!=1)
-				sol+=i.second;
-		for(auto i:b)
-			if(i.first!=1)
-				sol+=i.second;
-		cout<<sol<<endl;
+		for (auto i : a)
+			if (i.first != 1)
+				sol += i.second;
+		for (auto i : b)
+			if (i.first != 1)
+				sol += i.second;
+		cout << sol << endl;
 	}
 	return 0;
 }
