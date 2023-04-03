5a42f407958498bc1efde2f21c8f825d915474c8
ibrahim <ibrahim-debugger1>
2023-04-03 16:20:52 +0300

    nice idea

---
+++ 240. Search a 2D Matrix II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int i = n - 1, j = 0;
+        while (i >= 0 && j < m) {
+            if (matrix[i][j] == target)
+                return true;
+            else if (matrix[i][j] > target)
+                i -= 1;
+            else
+                j += 1;
+        }
+        return false;
+    }
+};
